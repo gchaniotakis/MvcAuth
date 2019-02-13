bfa8f0f81a9f0af671609739738abf157ce0c800
George Chaniotakis <haniotinho@gmail.com>
2019-02-13 21:07:18 +0200

    user model,login controller and view,user manager model

---
+++ MvcAuth/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAuth.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string[] Roles { get; set; }
+    }
+}
---
+++ MvcAuth/Models/UserManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAuth.Models
+{
+    public class UserManager
+    {
+        private List<User> _users = new List<User>();
+
+
+        private UserManager()
+        {
+            //add users: bill/ gates /admin, owner
+            // steve / ballmer / developers
+            //satya / nadella / ceo, admin
+            //share / point / developers
+        }
+
+        //public User Login (string username, string password)
+        //{
+
+        //}
+
+        //public bool UserExists (string username)
+        //{
+        //    return false;
+        //}
+    }
+}
